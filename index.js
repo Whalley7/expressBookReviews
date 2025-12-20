3a6199392109343c9f2257e1d43fbf4e0e2e7b78
Wendy Hallett <hallettwendy@gmail.com>
2025-12-19 22:35:03 -0500

    register user file

--- index.js
+++ index.js
@@ -34,28 +34,10 @@ app.use("/customer/auth/*", function auth(req,res,next){
 });
 
 // Register a new user
-app.post("/register", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
 
 
-    // Check if both username and password are provided
-    if (username && password) {
-        // Check if the user does not already exist
-        if (!doesExist(username)) {
-            // Add the new user to the users array
-            users.push({"username": username, "password": password});
-            return res.status(200).json({message: "User successfully registered. Now you can login"});
-        } else {
-            return res.status(404).json({message: "User already exists!"});
-        }
-    }
-    // Return error if username or password is missing
-    return res.status(404).json({message: "Unable to register user."});
-});
 
-
-const PORT = 5050;
+const PORT = 3500;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
